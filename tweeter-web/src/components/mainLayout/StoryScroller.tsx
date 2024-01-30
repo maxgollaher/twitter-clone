0a28440792ff33580c583fd977d0e686d2f8a90b
maxgollaher <max.c.gollaher@gmail.com>
2024-01-29 20:37:02 -0700

    Update StoryScroller.tsx

--- tweeter-web/src/components/mainLayout/StoryScroller.tsx
+++ tweeter-web/src/components/mainLayout/StoryScroller.tsx
@@ -61,38 +61,6 @@ const StoryScroller = () => {
     // TODO: Replace with the result of calling server
     return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
   };
-  const navigateToUser = async (event: React.MouseEvent): Promise<void> => {
-    event.preventDefault();
-
-    try {
-      let alias = extractAlias(event.target.toString());
-
-      let user = await getUser(authToken!, alias);
-
-      if (!!user) {
-        if (currentUser!.equals(user)) {
-          setDisplayedUser(currentUser!);
-        } else {
-          setDisplayedUser(user);
-        }
-      }
-    } catch (error) {
-      displayErrorMessage(`Failed to get user because of exception: ${error}`);
-    }
-  };
-
-  const extractAlias = (value: string): string => {
-    let index = value.indexOf("@");
-    return value.substring(index);
-  };
-
-  const getUser = async (
-    authToken: AuthToken,
-    alias: string
-  ): Promise<User | null> => {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.findUserByAlias(alias);
-  };
 
   return (
     <div className="container px-0 overflow-visible vh-100">
